172fa07984f8ed179eebbed241a0cabfc8bcf340
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2022-05-15 10:43:59 +0200

    INFRA: Added MAUI Project

---
+++ OSS-PhotoBooth.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OSS_PhotoBooth.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ OSS-PhotoBooth.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OSS_PhotoBooth.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
