691f66ba9d1cd4539a0837e8bd96809d09f186d7
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2022-05-15 10:59:37 +0200

    INFRA: Added Class Library Tests Project

---
+++ OSS-PhotBooth.Core.Tests.Unit/Foo.cs
@@ -0,0 +1,11 @@
+namespace OSS_PhotBooth.Core.Tests.Unit
+{
+    public class Foo
+    {
+        [Fact]
+        public void DeleteMe()
+        {
+            Assert.True(true);
+        }
+    }
+}
